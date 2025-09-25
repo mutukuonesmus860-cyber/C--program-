35f400af8f4caf791047961c93bee41ee4ad11c4
mutukuonesmus860-cyber <mutukuonesmus860@gmail.com>
2025-09-25 13:40:53 +0300

    Add files via upload

---
+++ your information .c
@@ -0,0 +1,25 @@
+/*
+Name:Mwania onesmus 
+Reg:CT100/G/26270/25
+Description:user information 
+*/
+
+#include<stdio.h>
+
+int main (){
+int height, weight;
+long long phone;
+
+printf ("Enter your height(in kg):\n");
+scanf ("%d",&height);
+
+printf ("Enter your weight (cm):\n");
+scanf ("%d",weight);
+
+printf ("Enter phone number:");
+scanf ("%ll",phone);
+
+return 0;
+}
+
+    
