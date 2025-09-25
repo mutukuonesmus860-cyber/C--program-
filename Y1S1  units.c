a0506d46b3e163b4f57f942078d87c834d2158cb
mutukuonesmus860-cyber <mutukuonesmus860@gmail.com>
2025-09-25 13:42:47 +0300

    Add files via upload

---
+++ Y1S1  units.c
@@ -0,0 +1,21 @@
+/*
+Name:Mwania onesmus 
+Reg:CT100/G/26270/25
+Description:IT UNITS Y1S1 
+*/
+
+#include<stdio.h>
+
+int main (){
+printf ("essential of accounting.\n");
+printf ("mathematics for science.\n");
+printf ("introduction to programming and algorithm. \n");
+printf ("communication skills. \n");
+printf ("fundamental of ICT. \n");
+printf ("computer system and organisation. \n");
+printf ("introduction to computer application. \n");
+printf ("analogue electronics. \n");
+return 0;
+}
+
+    
